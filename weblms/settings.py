8f19f0bce2318eec05b8efeb0b7f0d2ed1487685
Lucas Chuairi <lusacrj@gmail.com>
2011-10-16 19:50:36 -0200

    alterei uma bobeirinha

--- weblms/settings.py
+++ weblms/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'web.urls'
+ROOT_URLCONF = 'weblms.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
